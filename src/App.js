695d79ba014f4769171a5d94e821680eeb0dec7a
Perfect92 <reactionperfect9240@gmail.com>
2023-02-21 20:56:03 +0300

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor (props){
+    super(props)
+    this.state = {
+      name: 'ALEX', 
+      age: 35
+    }
+    this.btClick = this.btClick.bind(this)
+  }
+  btClick(){
+    this.setState({
+      name : 'Geeks',
+      age : 15
+    })
+  }
+  render() {
+  return(<div className="App">
+    <p>Имя: {this.state.name} , Возраст: {this.state.age} </p>
+    <button id="bt" onClick={this.btClick}>Нажми на меня</button> 
+  </div>)
+  }
 }
 
 export default App;
